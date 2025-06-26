695153bb085ccd49414163ef60575ada50c3bb12
MarkGamboa <202310110@fit.edu.ph>
2025-06-26 22:37:27 +0800

    feat: Add featured plants data for display in plant handler

---
+++ handlers/plant.handler.php
@@ -0,0 +1,51 @@
+<?php
+$featuredPlants = [
+    [
+        "name" => "Nebula Bloom",
+        "price" => 35,
+        "desc" => "A radiant flower that glows with the colors of cosmic clouds.",
+        "imgClass" => "nebula-bloom"
+    ],
+    [
+        "name" => "Lunar Cactus",
+        "price" => 28,
+        "desc" => "Tough and resilient, it absorbs moonlight to bloom at night.",
+        "imgClass" => "lunar-cactus"
+    ],
+    [
+        "name" => "Meteor Fern",
+        "price" => 42,
+        "desc" => "Feathers shimmer like meteors streaking through space dust.",
+        "imgClass" => "meteor-fern"
+    ],
+    [
+        "name" => "Solar Vine",
+        "price" => 30,
+        "desc" => "Golden tendrils that sway toward sunlight, storing solar energy.",
+        "imgClass" => "solar-vine"
+    ],
+    [
+        "name" => "Galaxy Orchid",
+        "price" => 55,
+        "desc" => "Petals with shifting star-like specks, a true interstellar marvel.",
+        "imgClass" => "galaxy-orchid"
+    ],
+    [
+        "name" => "Comet Ivy",
+        "price" => 26,
+        "desc" => "Fast-growing vine that leaves glowing trails in the dark.",
+        "imgClass" => "comet-ivy"
+    ],
+    [
+        "name" => "Asteroid Moss",
+        "price" => 18,
+        "desc" => "Spongy moss that thrives in zero-gravity and rocky terrain.",
+        "imgClass" => "asteroid-moss"
+    ],
+    [
+        "name" => "Venus Bell",
+        "price" => 33,
+        "desc" => "Bell-shaped bloom that hums gently when touched.",
+        "imgClass" => "venus-bell"
+    ]
+];
