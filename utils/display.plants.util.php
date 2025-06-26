40f25138f8b2da1e56a9dd7cc49ec4095a42af12
MarkGamboa <202310110@fit.edu.ph>
2025-06-26 23:21:12 +0800

    feat: Update displayPlants function to conditionally render plant images

--- utils/display.plants.util.php
+++ utils/display.plants.util.php
@@ -4,7 +4,11 @@ if (!function_exists('displayPlants')) {
     {
         foreach ($plants as $plant) {
             echo "<article class='plant-card' tabindex='0' aria-label='{$plant['name']} Plant'>";
-            echo "<div class='plant-image {$plant['imgClass']}' role='img' aria-hidden='true' title='{$plant['name']}'></div>";
+            if (!empty($plant['img'])) {
+                echo "<img src='{$plant['img']}' alt='{$plant['name']}' class='plant-image {$plant['imgClass']}' title='{$plant['name']}' />";
+            } else {
+                echo "<div class='plant-image {$plant['imgClass']}' role='img' aria-hidden='true' title='{$plant['name']}'></div>";
+            }
             echo "<h3 class='plant-name'>{$plant['name']}</h3>";
             echo "<p class='plant-description'>{$plant['desc']}</p>";
             echo "<p class='plant-price'>Price: {$plant['price']} Galactic Credits</p>";
