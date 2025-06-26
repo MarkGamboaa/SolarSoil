48b7af3e5d580eece776b05ec124b5680edf4078
MarkGamboa <202310110@fit.edu.ph>
2025-06-26 22:38:11 +0800

    feat: Add shop page with featured plants section and styling

---
+++ pages/shop/index.php
@@ -0,0 +1,20 @@
+<?php
+require VENDOR_PATH . 'autoload.php';
+require_once HANDLERS_PATH . 'plant.handler.php';
+require_once UTILS_PATH . 'display.plants.util.php';
+require_once COMPONENTS_PATH . 'templates/header.component.php';
+?>
+
+<head>
+    <link rel="stylesheet" href="/pages/shop/assets/css/style.css">
+</head>
+
+<section class="featured-plants container" aria-label="Featured Plants" id="products" tabindex="-1">
+    <h2 class="section-title">Featured Plants</h2>
+    <div class="plant-grid">
+        <?php displayPlants($featuredPlants); ?>
+    </div>
+</section>
+
+
+<?php require_once COMPONENTS_PATH . 'templates/footer.component.php'; ?>
