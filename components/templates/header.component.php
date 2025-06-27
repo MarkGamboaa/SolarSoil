b2c4a30f96d37c0621f9da5299f639060a7c8783
MarkGamboa <202310110@fit.edu.ph>
2025-06-27 10:37:09 +0800

    feat: Add missing favicon link in header component

--- components/templates/header.component.php
+++ components/templates/header.component.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" type="image/x-icon" href="/assets/img/Logo.png">
     <title>SolarSoil - Interstellar Plant Store</title>
     <link rel="stylesheet" href="/assets/css/style.css">
 </head>
